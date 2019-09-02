938606ee34e398c09ca3970b9211944ba101ea75
lauraunci <lauraunci@yahoo.com>
2019-09-02 12:26:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push("queen");
+console.log("User flipped queen");
+const cardTwo = cards[2];
+cardsInPlay.push("king");
+console.log("User flipped king");
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+	alert("Sorry, try again");
+}
+
